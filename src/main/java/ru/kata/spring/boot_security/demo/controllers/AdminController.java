39304dcbeb18fe3389b68cf103258577563087b1
Agutinka <alenaagutina24@gmail.com>
2024-01-20 19:51:08 +0300

    Продолжила добавлять стили. Готова страница admin, user, add new user. Частично реализованы, но ещё не доделаны страницы edit и delete

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,19 +39,39 @@ public class AdminController {
         return "admin";
     }
 
-    @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("role", roleService.getAllUser());
-        return "create";
-    }
-
     @PostMapping("/create")
-    public String addUser(@ModelAttribute("userForm") User userForm) {
-        userService.save(userForm);
+    public String addUser(@ModelAttribute(value = "user") User user) {
+        userService.save(user);
 
         return "redirect:/admin";
     }
+
+    @GetMapping("/create")
+    public String create(ModelMap modelMap) {
+        modelMap.addAttribute("user", new User());
+        modelMap.addAttribute("role", roleService.getAllUser());
+        return "create";
+    }
+
+//@GetMapping("/create")
+//public String create(Model model, Principal principal) {
+//    String currentUserEmail = principal.getName();
+//    User currentUser = userService.findUserByUsername(currentUserEmail);
+//
+//    model.addAttribute("currentUser", currentUser);
+//    model.addAttribute("user", new User());
+//    model.addAttribute("role", roleService.getAllUser());
+//
+//    return "create";
+//}
+//
+//
+//    @PostMapping("/create")
+//    public String addUser(@ModelAttribute("userForm") User userForm) {
+//        userService.save(userForm);
+//
+//        return "redirect:/admin";
+//    }
 
     @GetMapping("/edit")
     public String edit(@RequestParam(value = "id", required = false) Long id, Model model) {
